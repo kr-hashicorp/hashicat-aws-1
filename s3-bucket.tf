7545653176e118c8917db83a9a9b328b6bda8ebb
JS Park <jsp@hashicorp.com>
2021-11-18 17:28:23 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/jsp-training/s3-bucket/aws"
-  version = "2.10.0"
+  version = "2.7.0"
   # insert required variables here
   bucket_prefix = "jsp001"
 }
